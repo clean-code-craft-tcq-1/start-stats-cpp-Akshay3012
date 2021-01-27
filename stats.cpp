9a5e90df53ccc7c52861bf3da2f09ca0dc799335
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-01-27 21:56:40 +0530

    Updated the source file for 1st and 2nd test cases

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,33 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+class Statistics::ComputeStatistics {
+    private:
+        std::vector<float> computeStatistics;
+
+   public:
+    float maximum;
+    float minimum;
+    float average;
+   ComputeStatistics(const std::vector<float> &ar);
+};
+
+Statistics::ComputeStatistics::ComputeStatistics(const std::vector<float> &ar)
+{
+    float sum;
+    computeStatistics = ar;
+    
+    if(computeStatistics.size() == 0)
+    {
+    average = NAN;
+    minimum = NAN;
+    maximum = NAN;
+    }
+    else
+    {
+        minimum = *std::min_element(computeStatistics.begin(), computeStatistics.end());
+        maximum = *std::max_element(computeStatistics.begin(), computeStatistics.end());
+        for(std::size_t i = 0; i < computeStatistics.size(); i++)
+            sum += computeStatistics.at(i);
+            average =  sum/computeStatistics.size();
+    }
 }
