2f0a9e421c6bd438f8d249a0525f2831e555fb3b
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-01-27 22:30:38 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,7 +6,7 @@ using namespace std;
 namespace Statistics 
 {
  
-    ComputeStatistics::ComputeStatistics(const std::vector<float> &ar);
+    Statistics::ComputeStatistics::ComputeStatistics(const std::vector<float> &ar);
 }
 
 //namespace Statistics {
