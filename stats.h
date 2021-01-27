bfea0ded1f61040fff6259cd19cf3e9ed4077018
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-01-27 22:52:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,11 +3,17 @@
 #include <iostream>
 using namespace std;
 
-namespace Statistics 
-{
- 
-    Statistics::ComputeStatistics::ComputeStatistics(const std::vector<float> &ar);
+namespace Statistics {
+    class ComputeStatistics; 
 }
+class ComputeStatistics {
+   public:
+    std::vector<float> computeStatistics;
+    float maximum;
+    float minimum;
+    float average;
+   ComputeStatistics(const std::vector<float> &ar);
+};
 
 //namespace Statistics {
  //  Stats ComputeStatistics(const std::vector<___>& );
