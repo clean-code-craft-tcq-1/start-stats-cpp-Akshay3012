7f2fe7419999bc6a108ff71f3f0a4f7278167daa
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-01-28 21:55:15 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -28,3 +28,34 @@ Statistics::ComputeStatistics::ComputeStatistics(const std::vector<float> &ar)
         }
     }
 }
+
+StatsAlerter::StatsAlerter(const float maxThreshold, std::vector<IAlerter*> alerters)
+{
+    StatsAlerter* obj = new StatsAlerter(maxThreshold,alerters);
+    obj->maxThreshold = maxThreshold;
+    
+}
+
+void StatsAlerter::checkAndAlert(const std::vector<float> &ar)
+{
+    computeMax = ar;
+    float max_value = 0.0;
+    if(computeMax.size()!=0)
+    {
+        max_value = *std::max_element(computeMax.begin(), computeMax.end());
+    }
+    
+    //StatsAlerter obj_StatsAlerter ;
+    StatsAlerter* obj_StatsAlerter = new StatsAlerter(maxThreshold,alerters);
+    //StatsAlerter statsAlerter(maxThreshold, alerters);
+    EmailAlert obj_EmailAlert;
+    LEDAlert obj_LEDAlert;
+    
+    
+    if(max_value>obj_StatsAlerter->maxThreshold)
+    {
+        obj_EmailAlert.emailSent = true;
+        obj_LEDAlert.ledGlows =true;
+    }
+    
+}
