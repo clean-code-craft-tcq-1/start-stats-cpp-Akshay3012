d444d20de7e9a463694cf96e4f9e36d0373f0b5a
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-01-28 21:55:59 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -15,6 +15,36 @@ class ComputeStatistics {
 };
 }
 
+class IAlerter
+{
+    
+};
+
+class EmailAlert : public IAlerter
+{
+    public:
+        bool emailSent;
+};
+
+class LEDAlert : public IAlerter
+{
+    public:
+        bool ledGlows;
+};
+
+class StatsAlerter
+{
+    public:
+    float maxThreshold;
+    EmailAlert emailAlert;
+    LEDAlert ledAlert;
+    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    std::vector<float> computeMax;
+    //StatsAlerter();
+    StatsAlerter(const float maxThreshold, std::vector<IAlerter*> alerters);
+    void checkAndAlert(const std::vector<float> &ar);
+};
+
 //namespace Statistics {
  //  Stats ComputeStatistics(const std::vector<___>& );
 //}
